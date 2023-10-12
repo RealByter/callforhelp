29e234fd74e2088d926ecc7ed148d05f3710930a
yaeledri <yaeledri@carmel6000.amitnet.org>
2023-10-12 18:50:05 +0300

    added router and default pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,33 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import SocketProvider from "./context/socket/SocketProvider.tsx";
+import { PageExample } from "./pages/PageExample";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // children: [
+    //   {
+    //     path: "/ex",
+    //     element: <PageExample />,
+    //     // loader: teamLoader,
+    //   },
+    // ],
+  },{
+    path: "/ex",
+    element: <PageExample />,
+    // loader: teamLoader,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <SocketProvider>
-      <App />
+      <RouterProvider router={router} />
     </SocketProvider>
   </React.StrictMode>
 );
