41bee41ead9aa11e71ab88bc7bfa7456f660577e
yaeledri <yaeledri@carmel6000.amitnet.org>
2023-10-12 18:50:05 +0300

    added router and default pages

---
+++ client/src/pages/PageExample.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { CompExample } from '../components/CompExample'
+
+export const PageExample = () => {
+    return (
+        <div>
+            This is a page example
+            <CompExample />
+        </div>
+    )
+}
