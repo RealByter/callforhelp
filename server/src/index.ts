b78d3350b886a5c20655f0016f76208e3bba5ff4
Romeo Yofe <mrborder3960@gmail.com>
2023-10-12 18:08:07 +0300

    fix(index.ts, server.ts): Make type for connection callback and pass io server in it too

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { CallForHelpServer } from './server';
 
-const onConnect = (socket: Socket): void => {
+export type SocketFunction = (io: Server, socket: Socket) => void;
+
+const onConnect: SocketFunction = (io: Server, socket: Socket): void => {
      // register handlers here, see https://socket.io/docs/v4/server-application-structure/ for structure
 }
 
