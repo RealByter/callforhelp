1ce98cf056fed57b4d38eb09ae8bd8fa3ef6e35f
Nitzan Yizhar <nitzanyizhar@gmail.com>
2023-10-13 17:41:13 +0300

    Added hint component for the form field
    Implemented some of the functionality of the registration form

--- client/src/constants/validationOptions.tsx
+++ client/src/constants/validationOptions.tsx
@@ -1,16 +1,27 @@
 export const nameValidations = {
-    required: true,
-    min: 2,
+    required: "נדרש להכניס שם",
+    minLength: {
+        value: 2,
+        message: "צריך להיות לפחות 2 תווים",
+    },
 };
 export const emailValidations = {
-    required: true,
+    required: "צריך להכניס מייל",
     // pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/,
-    pattern: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+    pattern: {
+        value: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        message: "אימייל לא בתבנית נכונה",
+    },
 };
 
 export const passwordValidations = {
-    required: true,
-    min: 8,
-    pattern:
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    required: "צריך להכניס סיסמא",
+    minLength: {
+        value: 8,
+        message: "צריך להיות לפחות 8 תווים",
+    },
+    pattern: {
+        value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+        message: "סיסמא לא תקינה",
+    },
 };
