2f63badd0c93c903144588e48deca0f7c0778c30
RealByter <justavishay@gmail.com>
2023-10-14 14:39:38 +0300

    complete example form

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,29 +1,36 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import SocketProvider from "./context/socket/SocketProvider.tsx";
-import SignUpPage from "./pages/SignUp.tsx";
-import SignInPage from "./pages/SignIn.tsx";
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import SocketProvider from './context/socket/SocketProvider.tsx';
+import SignUpPage from './pages/SignUp.tsx';
+import SignInPage from './pages/SignIn.tsx';
+import ExampleForm from './components/ExampleForm.tsx';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },{
-    path: "/signup",
-    element: <SignUpPage />,
-  }, {
-    path: "/signin",
-    element: <SignInPage />
-  }
+	{
+		path: '/',
+		element: <App />,
+	},
+	{
+		path: '/signup',
+		element: <SignUpPage />,
+	},
+	{
+		path: '/signin',
+		element: <SignInPage />,
+	},
+	{
+		path: '/test',
+		element: <ExampleForm />,
+	},
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <SocketProvider>
-      <RouterProvider router={router} />
-    </SocketProvider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<SocketProvider>
+			<RouterProvider router={router} />
+		</SocketProvider>
+	</React.StrictMode>
 );
