ca73bb59093f1a497b6b10f9b87df7b37677cdba
RealByter <justavishay@gmail.com>
2023-10-14 17:14:13 +0300

    format client

--- client/src/components/FormField.tsx
+++ client/src/components/FormField.tsx
@@ -1,52 +1,39 @@
-import React from "react";
-import Eye from "../assets/eye.svg";
-import NoShowEye from "../assets/eye-slash.svg";
-import "../styles/forms.scss";
-import { UseFormRegisterReturn } from "react-hook-form";
+import React from 'react';
+import Eye from '../assets/eye.svg';
+import NoShowEye from '../assets/eye-slash.svg';
+import '../styles/forms.scss';
+import { UseFormRegisterReturn } from 'react-hook-form';
 type TextInputProps = {
-    label: string;
-    inputType?: string;
-    placeHolder?: string;
-    inputProps: UseFormRegisterReturn; // Should have the useForm register props
-    inputClass?: string;
+  label: string;
+  inputType?: string;
+  placeHolder?: string;
+  inputProps: UseFormRegisterReturn; // Should have the useForm register props
+  inputClass?: string;
 };
 
 export default function FormField(props: TextInputProps) {
-    const {
-        label,
-        inputType = "text",
-        inputProps,
-        inputClass = "",
-        placeHolder = label,
-    } = props;
-    const [shown, setShown] = React.useState(inputType !== "password");
-    const toggleShown = () => {
-        setShown((prev) => !prev);
-    };
-    const eye = shown ? NoShowEye : Eye;
-    const effectiveInputType =
-        inputType !== "password" ? inputType : shown ? "text" : "password";
-    return (
-        <div className="form-field">
-            <label htmlFor={`input-${inputProps.name}`}>{label}</label>
-            <div className="form-field-input-container">
-                <input
-                    {...inputProps!}
-                    id={`input-${inputProps.name}`}
-                    type={effectiveInputType}
-                    className={`form-field-input ${inputClass}`}
-                    placeholder={placeHolder}
-                />
-                {inputType === "password" && (
-                    <img
-                        className="form-field-eye"
-                        width={24}
-                        height={24}
-                        src={eye}
-                        onClick={toggleShown}
-                    />
-                )}
-            </div>
-        </div>
-    );
+  const { label, inputType = 'text', inputProps, inputClass = '', placeHolder = label } = props;
+  const [shown, setShown] = React.useState(inputType !== 'password');
+  const toggleShown = () => {
+    setShown((prev) => !prev);
+  };
+  const eye = shown ? NoShowEye : Eye;
+  const effectiveInputType = inputType !== 'password' ? inputType : shown ? 'text' : 'password';
+  return (
+    <div className="form-field">
+      <label htmlFor={`input-${inputProps.name}`}>{label}</label>
+      <div className="form-field-input-container">
+        <input
+          {...inputProps!}
+          id={`input-${inputProps.name}`}
+          type={effectiveInputType}
+          className={`form-field-input ${inputClass}`}
+          placeholder={placeHolder}
+        />
+        {inputType === 'password' && (
+          <img className="form-field-eye" width={24} height={24} src={eye} onClick={toggleShown} />
+        )}
+      </div>
+    </div>
+  );
 }
--- client/src/components/Hint.tsx
+++ client/src/components/Hint.tsx
@@ -1,26 +1,24 @@
-import React from "react";
-import "../styles/forms.scss";
+import React from 'react';
+import '../styles/forms.scss';
 
 export type HintProps = {
-    messages?: string | string[];
-    component?: React.JSX.Element;
+  messages?: string | string[];
+  component?: React.JSX.Element;
 };
 
 const Hint = (props: HintProps) => {
-    const { messages, component } = props;
-    const content = Array.isArray(messages) ? (
-        messages.map((item) => (
-            <p key={`hint-${item}${Math.random().toString()}`}>{item}</p>
-        ))
-    ) : (
-        <p>{messages}</p>
-    );
-    return (
-        <div className="form-field-hint">
-            {messages && content}
-            {component && component}
-            <div className="form-field-hint-triangle"></div>
-        </div>
-    );
+  const { messages, component } = props;
+  const content = Array.isArray(messages) ? (
+    messages.map((item) => <p key={`hint-${item}${Math.random().toString()}`}>{item}</p>)
+  ) : (
+    <p>{messages}</p>
+  );
+  return (
+    <div className="form-field-hint">
+      {messages && content}
+      {component && component}
+      <div className="form-field-hint-triangle"></div>
+    </div>
+  );
 };
 export default Hint;
