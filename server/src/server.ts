9cbf4e513ccd55d468eb8d3121b952e83171edb3
Asaf Cohen <81377532+asafcohen98@users.noreply.github.com>
2023-10-14 16:36:02 +0300

    Add format script on server + write all

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,52 +2,62 @@ import { Socket, Server } from 'socket.io';
 import { SocketFunction } from './index';
 
 export class CallForHelpServer {
-    public static readonly PORT: string = '3000';
-    public static readonly CORS_ORIGIN = 'http://localhost:5173';
+  public static readonly PORT: string = '3000';
+  public static readonly CORS_ORIGIN = 'http://localhost:5173';
 
-    private onConnectCallback: SocketFunction;
-    private port: number;
-    private corsOrigin: string;
-    private io: Server;
+  private onConnectCallback: SocketFunction;
+  private port: number;
+  private corsOrigin: string;
+  private io: Server;
 
-    constructor(onConnectCallback: SocketFunction) {
-        this.onConnectCallback = onConnectCallback;
-        this.configurate();
-        this.createServerSocket();
-        this.listen();
+  constructor(onConnectCallback: SocketFunction) {
+    this.onConnectCallback = onConnectCallback;
+    this.configurate();
+    this.createServerSocket();
+    this.listen();
+  }
+
+  private configurateSetting(
+    settingEnvName: string,
+    defaultSetting: string
+  ): string {
+    const setting = process.env[settingEnvName];
+
+    if (setting === undefined) {
+      console.log(
+        `Environment variable ${settingEnvName} is non-existent! defaulting to ${defaultSetting}`
+      );
+
+      return defaultSetting;
     }
 
-    private configurateSetting(settingEnvName: string, defaultSetting: string): string {
-        const setting = process.env[settingEnvName];
+    return setting;
+  }
 
-        if (setting === undefined) {
-            console.log(`Environment variable ${settingEnvName} is non-existent! defaulting to ${defaultSetting}`);
-            
-            return defaultSetting;
-        } 
+  private configurate(): void {
+    this.port = parseInt(
+      this.configurateSetting('PORT', CallForHelpServer.PORT)
+    );
+    this.corsOrigin = this.configurateSetting(
+      'CORS_ORIGIN',
+      CallForHelpServer.CORS_ORIGIN
+    );
+  }
 
-        return setting;
-    }
+  private createServerSocket(): void {
+    this.io = new Server(this.port, {
+      cors: {
+        origin: [this.corsOrigin]
+      }
+    });
+  }
 
-    private configurate(): void { 
-        this.port = parseInt(this.configurateSetting('PORT', CallForHelpServer.PORT));
-        this.corsOrigin = this.configurateSetting('CORS_ORIGIN',  CallForHelpServer.CORS_ORIGIN);
-    }
+  private listen(): void {
+    console.log(`Listening for connections on ${this.port}`);
 
-    private createServerSocket(): void {
-        this.io = new Server(this.port, {
-            cors: {
-                origin: [this.corsOrigin]
-            }
-        });
-    }
-    
-    private listen(): void {
-        console.log(`Listening for connections on ${this.port}`)
-
-        this.io.on('connection', (socket: Socket) => {
-            console.log(`New connection from ${socket.handshake.address}`)
-            this.onConnectCallback(this.io, socket);
-        });
-    }
+    this.io.on('connection', (socket: Socket) => {
+      console.log(`New connection from ${socket.handshake.address}`);
+      this.onConnectCallback(this.io, socket);
+    });
+  }
 }
