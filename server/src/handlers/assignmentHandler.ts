4c5335aadf88e7ea3b8c0706e4ffe9659d2dc95a
RealByter <justavishay@gmail.com>
2023-10-18 21:07:15 +0300

    create an empty handler for the assignment process

---
+++ server/src/handlers/assignmentHandler.ts
@@ -0,0 +1,5 @@
+import { Server, Socket } from "socket.io";
+
+module.exports = (io: Server, socket: Socket) => {
+  
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,11 @@ import { CallForHelpServer } from './server';
 
 export type SocketFunction = (io: Server, socket: Socket) => void;
 
+const registerAssignmentHandlers = require('./handlers/assignmentHandler');
+
 const onConnect: SocketFunction = (io: Server, socket: Socket): void => {
   // register handlers here, see https://socket.io/docs/v4/server-application-structure/ for structure
+  registerAssignmentHandlers(io, socket);
 };
 
 const app = new CallForHelpServer(onConnect);
