dc3076862c5879eb9aae46926965620c80db623b
gertzMan <101058145+gertzMan@users.noreply.github.com>
2023-10-19 21:00:31 +0300

    added an agreement pop up component that has 2 modes of action

---
+++ client/src/components/AgreementPopup.tsx
@@ -0,0 +1,83 @@
+/* 
+this is a pop component that accepts a prop called agreeNeeded. 
+if agreeNeeded is true, the user must read the whole content of 
+the popup, and push a button acknowledging he read it. 
+if agreeNeeded is false, the user can just press the x 
+button to close the popup
+
+upon implementation - insert correct texts and implement 
+the handleAgree function
+*/
+
+import { FC, useEffect, useRef, useState } from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
+
+interface AgreementPopupProps {
+  agreeNeeded: boolean;
+}
+
+const AgreementPopup: FC<AgreementPopupProps> = ({ agreeNeeded }) => {
+  const [showPopUp, setShowPopUp] = useState<boolean>(true);
+  const [buttonEnabled, setButtonEnabled] = useState<boolean>(false);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const contentText: Array<String> = [
+    'אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפאני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ אני התוכן שמספיק ללחוץ איקס כדי לסגור את הפופאפ',
+    ' אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשראני התוכן שחייב לאשר אני התוכן שחייב לאשראני התוכן שחייב לאשר אני התוכן שחייב לאשראני התוכן שחייב לאשר אני התוכן שחייב לאשראני התוכן שחייב לאשר אני התוכן שחייב לאשראני התוכן שחייב לאשר אני התוכן שחייב לאשראני התוכן שחייב לאשר אני התוכן שחייב לאשראני התוכן שחייב לאשר אני התוכן שחייב לאשראני התוכן שחייב לאשר אני התוכן שחייב לאשראני התוכן שחייב לאשר אני התוכן שחייב לאשראני התוכן שחייב לאשר אני התוכן שחייב לאשראני התוכן שחייב לאשר אני התוכן שחייב לאשראני התוכן שחייב לאשר אני התוכן שחייב לאשראני התוכן שחייב לאשר אני התוכן שחייב לאשראני התוכן שחייב לאשר אני התוכן שחייב לאשראני התוכן שחייב לאשר אני התוכן שחייב לאשראני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשראני התוכן שחייב לאשר אני התוכן שחייב לאשר אני התוכן שחייב לאשר'
+  ];
+
+  const handleClose = () => {
+    setShowPopUp(false);
+  };
+
+  const handleAgree = () => {
+    //enter logic for clicking agree
+  };
+
+  useEffect(() => {
+    const element = contentRef.current;
+    if (element && element.scrollHeight <= element.clientHeight) {
+      setButtonEnabled(true);
+    }
+  }, [contentRef]);
+
+  const handleScroll = (e: React.UIEvent<HTMLElement>) => {
+    const element = e.currentTarget;
+    if (element.scrollHeight - element.scrollTop === element.clientHeight) {
+      setButtonEnabled(true);
+    }
+  };
+
+  return (
+    <>
+      {showPopUp && (
+        <div className="pop-up-background">
+          <div dir="rtl" className="pop-up-window">
+            {!agreeNeeded && (
+              <AiOutlineClose
+                className="pop-up-window__close-btn"
+                onClick={handleClose}
+                title="סגור"
+              />
+            )}
+            <p className="pop-up-window__content" onScroll={handleScroll} ref={contentRef}>
+              {contentText[agreeNeeded ? 1 : 0]}
+            </p>
+            {agreeNeeded && (
+              <div className="pop-up-window--bottom-nav">
+                <button
+                  className="pop-up-window--bottom-nav__agree-btn"
+                  onClick={handleAgree}
+                  disabled={!buttonEnabled}>
+                  אני מסכים לתנאים
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AgreementPopup;
