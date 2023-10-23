d7d18001443dc520f1f8777108c01b426e34dcce
RealByter <justavishay@gmail.com>
2023-10-23 16:24:14 +0300

    add closing functionality

--- client/src/components/InfoModal.tsx
+++ client/src/components/InfoModal.tsx
@@ -5,41 +5,41 @@ type InfoModalProps = {
   title: string;
   subtext: string;
   children: React.ReactNode;
+  onClose: () => void;
 };
 
-const CloseIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    fill="none"
-    className="close-button">
-    <path
-      d="M18 6L6 18"
-      stroke="#0E1C74"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-    <path
-      d="M6 6L18 18"
-      stroke="#0E1C74"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-  </svg>
-);
-
-const InfoModal: React.FC<InfoModalProps> = ({ title, subtext, children }) => {
+const InfoModal: React.FC<InfoModalProps> = ({ title, subtext, children, onClose }) => {
   return (
     <div className="info-modal">
-      <CloseIcon />
+      <svg
+        className="close-button"
+        onClick={onClose}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none">
+        <path
+          d="M18 6L6 18"
+          stroke="#0E1C74"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M6 6L18 18"
+          stroke="#0E1C74"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
       <h2 className="title">{title}</h2>
       <p className="subtext">{subtext}</p>
       {children}
-      <Button className="continue-button">הבנתי, אפשר להמשיך</Button>
+      <Button className="continue-button" onClick={onClose}>
+        הבנתי, אפשר להמשיך
+      </Button>
     </div>
   );
 };
