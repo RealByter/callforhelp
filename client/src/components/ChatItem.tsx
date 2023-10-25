6215b062417725ca2deee745ee00ed86dfd033b2
roniash101 <ashk.roni@gmail.com>
2023-10-26 00:09:07 +0300

    rename field to lastMessageSentAt

--- client/src/components/ChatItem.tsx
+++ client/src/components/ChatItem.tsx
@@ -4,24 +4,25 @@ import OrBackground from '../assets/OrBackground.svg';
 
 export interface ChatItemProps {
     name: string,
-    lastMessageTiming: string,
+    lastMessageSentAt: string,
     unreadMessages: number,
     isEnded: boolean,
     chatId: string
 };
 
 export const ChatItem: React.FC<ChatItemProps> = ({
-    name, lastMessageTiming, unreadMessages, isEnded, chatId }: ChatItemProps) => {
+    name, lastMessageSentAt, unreadMessages, isEnded, chatId }: ChatItemProps) => {
 
     const OnItemClick = () => {
-        // go into the right chat
+        // todo: go into the right chat
+        // navigate('/chat', { state: { companionName: myCompanions, chatId: myChats, role } });
     }
 
     return (
         <div className='chat-item' onClick={OnItemClick}>
             <div className='content'>
                 <span className='name'>{name}</span>
-                <span className='last-message'>תגובה אחרונה <span>{lastMessageTiming}</span>
+                <span className='last-message'>תגובה אחרונה <span>{lastMessageSentAt}</span>
                 </span>
             </div>
 
--- client/src/mock-data/chats-mock-data.ts
+++ client/src/mock-data/chats-mock-data.ts
@@ -1,35 +1,35 @@
 export const MOCK_CHATS = [
     {
         name: "הארי פוטר",
-        lastMessageTiming: "00:00",
+        lastMessageSentAt: "00:00",
         unreadMessages: 3,
         isEnded: false,
         chatId: "1"
     },
     {
         name: "רון וויזלי",
-        lastMessageTiming: "אתמול",
+        lastMessageSentAt: "אתמול",
         unreadMessages: 1,
         isEnded: false,
         chatId: "2"            
     },
     {
         name: "הרמיוני גריינג'ר",
-        lastMessageTiming: "02/03",
+        lastMessageSentAt: "02/03",
         unreadMessages: 0,
         isEnded: true,
         chatId: "3"            
     },
     {
         name: "נוויל לונגבוטום",
-        lastMessageTiming: "אתמול",
+        lastMessageSentAt: "אתמול",
         unreadMessages: 0,
         isEnded: true,
         chatId: "4"            
     },
     {
         name: "לונה לאבגוד",
-        lastMessageTiming: "15:56",
+        lastMessageSentAt: "15:56",
         unreadMessages: 3,
         isEnded: true,
         chatId: "5"           
