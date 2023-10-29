7d42948800840a5d698ec26757eeddeccf42a80a
RealByter <justavishay@gmail.com>
2023-10-29 08:47:17 +0200

    fixed chat top bar

--- client/src/components/ChatTopBar.tsx
+++ client/src/components/ChatTopBar.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import CloseIcon from '@mui/icons-material/Close';
 
 interface IChatTopBarProps {
   isChatEnded: boolean;
@@ -17,31 +16,47 @@ export const ChatTopBar: React.FC<IChatTopBarProps> = ({
   endChat,
   changeChatRoom,
   goBackToChatsPage
-}: IChatTopBarProps) => {  
+}: IChatTopBarProps) => {
   return (
     <div className={`chat-top-bar ${isChatEnded ? 'ended' : ''}`}>
       <div className="chat-top-bar-upper">
+        <button className="top-bar-close" onClick={goBackToChatsPage}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none">
+            <path
+              d="M18 6L6 18"
+              stroke="#0E1C74"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M6 6L18 18"
+              stroke="#0E1C74"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
         <div className="top-bar-text">
-          <span className="title">{companionName || "התומך המאושר"}</span>
+          <span className="title">{companionName || 'התומך המאושר'}</span>
           <span className="sub-title">אני פה בשבילך</span>
         </div>
-        <button className="top-bar-close" onClick={goBackToChatsPage}>
-          <CloseIcon />
-        </button>
       </div>
       <div className="chat-top-bar-lower">
         {isChatEnded ? (
           <p>השיחה הסתיימה</p>
         ) : (
-            <div className="chat-top-bar-lower-buttons">
-              <button onClick={changeChatRoom}>
-                {isSupporter ? 'איתור נתמך נוסף' : 'החלף תומך'}
-              </button>
-              <button onClick={endChat}>
-                סיום שיחה
-              </button>
-            </div>
-          )}
+          <div className="chat-top-bar-lower-buttons">
+            {isSupporter ? <></> : <button onClick={changeChatRoom}>החלף תומך</button>}
+            <button onClick={endChat}>סיום שיחה</button>
+          </div>
+        )}
       </div>
     </div>
   );
