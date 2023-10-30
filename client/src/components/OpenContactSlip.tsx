cc5761ee9e33979a53ae800e3bd2acf5ebcffc39
RealByter <justavishay@gmail.com>
2023-10-30 14:55:41 +0200

    added drag functionality to the slip

--- client/src/components/OpenContactSlip.tsx
+++ client/src/components/OpenContactSlip.tsx
@@ -1,18 +1,22 @@
+import Draggable from 'react-draggable';
+
 const OpenContactSlip: React.FC = () => {
   return (
-    <button className="open-contact-slip">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none">
-        <path
-          d="M21.6 0H2.4C1.08 0 0.012 1.08 0.012 2.4L0 24L4.8 19.2H21.6C22.92 19.2 24 18.12 24 16.8V2.4C24 1.08 22.92 0 21.6 0ZM21.6 16.8H3.804L3.096 17.508L2.4 18.204V2.4H21.6V16.8ZM10.8 12H13.2V14.4H10.8V12ZM10.8 4.8H13.2V9.6H10.8V4.8Z"
-          fill="white"
-        />
-      </svg>
-    </button>
+    <Draggable axis='y' defaultPosition={{x: 0, y: 360}}>
+      <button className="open-contact-slip">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none">
+          <path
+            d="M21.6 0H2.4C1.08 0 0.012 1.08 0.012 2.4L0 24L4.8 19.2H21.6C22.92 19.2 24 18.12 24 16.8V2.4C24 1.08 22.92 0 21.6 0ZM21.6 16.8H3.804L3.096 17.508L2.4 18.204V2.4H21.6V16.8ZM10.8 12H13.2V14.4H10.8V12ZM10.8 4.8H13.2V9.6H10.8V4.8Z"
+            fill="white"
+          />
+        </svg>
+      </button>
+    </Draggable>
   );
 };
 
