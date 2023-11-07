cd54d06c1f4633dc78b364b46ec52c83f7aec8ca
yaeledri <yaeledri@carmel6000.amitnet.org>
2023-11-07 22:50:44 +0200

    fixed scrolling bug

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -17,12 +17,6 @@ import {
 } from '../helpers/chatFunctions';
 import SupporteeWaiting from '../components/SupporteeWaiting';
 
-/*
-  TODO - take care of the disconnect events
-  TODO - take care of cases in which the chat id doesnt exist
-  TODO - leave socket room on exiting
-*/
-
 export const Chat = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const chatId = searchParams.get('chatId');
@@ -35,6 +29,9 @@ export const Chat = () => {
   const [user, userLoading] = useAuthState(auth);
   const scrollingRef = useRef(null);
   const navigate = useNavigate();
+
+  const [msgDiff, setMsgDiff] = useState<number>(0);
+  const [msgLength, setMsgLength] = useState<number>(messages?.length || 0);
 
   useEffect(() => {
     // redirect if user not logged in
@@ -69,11 +66,53 @@ export const Chat = () => {
   }, [chat, user, navigate]);
 
   useEffect(() => {
-    // scroll to bottom of the chat when getting a new msg
+    // check if the scrolling element is in viewport
+    function isInViewport(element: HTMLElement) {
+      const rect = element.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        (rect.bottom - 50) <= (window.innerHeight || document.documentElement.clientHeight)
+      );
+    }
     if (scrollingRef.current) {
-      (scrollingRef.current as HTMLElement).scrollIntoView({ behavior: 'smooth' });
+      if (isInViewport(scrollingRef.current)) {
+        scrollToBottom('smooth');
+        setMsgDiff(0);
+      } else {
+        // if now in viewport - add button to scroll down
+        setMsgLength(prev => {
+          const diff = (messages?.length || 0) - prev;
+          setMsgDiff(prev => prev + diff);
+          return messages?.length || 0;
+        })
+      }
     }
   }, [messages]);
+
+  useEffect(() => {
+    // immediately scroll down when entering the page
+    if (scrollingRef.current) {
+      scrollToBottom('instant');
+    }
+  }, [scrollingRef.current]);
+
+  const handleScroll = (e: React.UIEvent<HTMLElement>) => {
+    const element = e.currentTarget;
+    if (element.scrollHeight - element.scrollTop - 1 <= element.clientHeight) {
+      setMsgDiff(0);
+    }
+  };
+
+  const scrollToBottom = (behavior: 'smooth' | 'instant' | 'auto') => {
+    (scrollingRef.current as HTMLElement).scrollIntoView({ behavior });
+    setMsgLength(messages.length);
+  }
+
+  const scrollDownClick = () => {
+    if (scrollingRef.current) {
+      scrollToBottom('smooth');
+    }
+  };
 
   // get current date in IOS string
   const getCurrDateIsrael = () => {
@@ -132,28 +171,31 @@ export const Chat = () => {
         goBackToChatsPage={goBackToChatsPage}
       />
 
-      <div className="messages">
+      <div className="messages" onScroll={handleScroll}>
         {noMessages ? (
           <span className="no-msg">{companionName ? 'עוד אין הודעות' : 'עוד אין שותף'}</span>
         ) : (
-          messages!
-            .sort((a, b) => {
-              const aDate = new Date(a.date);
-              const bDate = new Date(b.date);
-              return aDate.getTime() - bDate.getTime();
-            })
-            .map((m, index) => (
-              <Message
-                key={index}
-                isSender={m.senderId === user!.uid}
-                content={m.content}
-                messageDate={m.date}
-                messageState={m.status}
-              />
-            ))
-        )}
+            messages!
+              .sort((a, b) => {
+                const aDate = new Date(a.date);
+                const bDate = new Date(b.date);
+                return aDate.getTime() - bDate.getTime();
+              })
+              .map((m, index) => (
+                <Message
+                  key={index}
+                  isSender={m.senderId === user!.uid}
+                  content={m.content}
+                  messageDate={m.date}
+                  messageState={m.status}
+                />
+              ))
+          )}
         <span ref={scrollingRef}></span>
       </div>
+      <div className="has-messages"
+        onClick={scrollDownClick}
+        style={{ display: msgDiff ? "flex" : "none" }}>{msgDiff}</div>
 
       <ChatBox sendChatMsg={sendMsg} disabled={!companionName || chat?.status === 'ended'} />
     </div>
