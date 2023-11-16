ee352d61975ca3b24a7316342e2161237362f23e
RealByter <justavishay@gmail.com>
2023-11-17 01:17:01 +0200

    complete the sign up firebase cloud function

--- client/src/firebase/functions/src/index.ts
+++ client/src/firebase/functions/src/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-throw-literal */
+/* eslint-disable linebreak-style */
 /* eslint-disable max-len */
 /**
  * Import function triggers from their respective submodules:
@@ -8,60 +10,98 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onCall, HttpsError} from "firebase-functions/v2/https";
+import * as functions from "firebase-functions";
 import * as logger from "firebase-functions/logger";
 import * as admin from "firebase-admin";
+import * as serviceAccount from "./callforhelp-37002-firebase-adminsdk-7ivya-e3e03f01cb.json";
 
-const serviceAccountPath = process.env.SERVICE_ACCOUNT_PATH;
-const databaseUrl = process.env.DATABASE_URL;
+const DATABASE_URL = "https://callforhelp-37002-default-rtdb.firebaseio.com";
 
-if (!serviceAccountPath) {
-  throw new Error("SERVICE_ACCOUNT_PATH environment variable is not set.");
-}
-if (!databaseUrl) {
-  throw new Error("SERVICE_ACCOUNT_PATH environment variable is not set.");
-}
-
-let serviceAccount: admin.ServiceAccount;
-
-try {
-  serviceAccount = require(serviceAccountPath);
-} catch (error: unknown) {
-  const err = error as {message: string};
-  throw new Error(`Error loading service account file: ${err.message}`);
-}
-
-admin.initializeApp({credential: admin.credential.cert(serviceAccount), databaseURL: databaseUrl});
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  databaseURL: DATABASE_URL,
+});
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const signUp = onCall((request) => {
-  const {email, password, name}: {email?: string, password?: string, name?: string} = request.data;
+export const signUp = functions.https.onCall(async (request) => {
+  const {email, password, name}: { email?: string; password?: string; name?: string } =
+    request.data;
 
   if (!email || !password || !name) {
-    throw new HttpsError("invalid-argument", "The function requires an email, a password and a name");
+    logger.error("The function requires an email, a password and a name: " + request);
+    throw new functions.https.HttpsError(
+      "invalid-argument",
+      "The function requires an email, a password and a name"
+    );
   }
 
   if (request.auth) {
-    throw new HttpsError("failed-precondition", "The user must be unauthenticated in order to sign up");
+    logger.error("The user must be unauthenticated in order to sign up: " + request);
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The user must be unauthenticated in order to sign up"
+    );
   }
 
-  const passwordRegex = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_])[A-Za-z\d@$!%*?&_]{8,20}$/);
+  const passwordRegex = new RegExp(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_])[A-Za-z\d@$!%*?&_]{8,20}$/
+  );
   const emailRegex = new RegExp(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/);
 
-  if (!passwordRegex.test(password)) {
-    throw new HttpsError("invalid-argument", "The password should include at least 8 characters, 1 uppercase letter, 1 lowercase letter, 1 number and one symbol from the following: @$!%*?&_");
+  try {
+    if (!passwordRegex.test(password)) {
+      logger.error("The password should include at least 8 characters, 1 uppercase letter, 1 lowercase letter, 1 number and one symbol from the following: @$!%*?&_: " + request);
+      throw new functions.https.HttpsError(
+        "invalid-argument",
+        "The password should include at least 8 characters, 1 uppercase letter, 1 lowercase letter, 1 number and one symbol from the following: @$!%*?&_"
+      );
+    }
+  } catch (error) {
+    logger.error("Error checking regex: " + error + ", " + request);
+    throw new functions.https.HttpsError("internal", "Error checking regex: " + error);
   }
   if (password.length < 8 || password.length > 20) {
-    throw new HttpsError("invalid-argument", "The password must have at least 8 characters and no more than 20");
+    throw new functions.https.HttpsError(
+      "invalid-argument",
+      "The password must have at least 8 characters and no more than 20"
+    );
   }
 
-  if (!emailRegex) {
-    throw new HttpsError("invalid-argument", "The email must be a valid address");
+  try {
+    if (!emailRegex) {
+      logger.error("The email must be a valid address: " + request);
+      throw new functions.https.HttpsError("invalid-argument", "The email must be a valid address");
+    }
+  } catch (error) {
+    logger.error("Error checking regex: " + error + ", " + request);
+    throw new functions.https.HttpsError("internal", "Error checking regex: " + error);
   }
 
   if (name.length < 2) {
-    throw new HttpsError("invalid-argument", "The name must include at least 2 characters");
+    logger.error("The name must include at least 2 characters: " + request);
+    throw new functions.https.HttpsError("invalid-argument", "The name must include at least 2 characters");
+  }
+
+  try {
+    const userRecord = await admin.auth().createUser({email, password});
+    const usersCollection = admin.firestore().collection("users");
+
+    try {
+      await usersCollection.doc(userRecord.uid).set({name, acceptedTerms: true});
+      try {
+        await admin.auth().updateUser(userRecord.uid, {displayName: name});
+      } catch (error) {
+        await usersCollection.doc(userRecord.uid).delete();
+        throw ""; // to get the the error throw below
+      }
+    } catch (error) {
+      await admin.auth().deleteUser(userRecord.uid);
+      throw ""; // to get to the actual error throw
+    }
+  } catch (error) {
+    logger.error("An error occurred while creating the user: " + request);
+    throw new functions.https.HttpsError("internal", "An error occurred while creating the user");
   }
 });
