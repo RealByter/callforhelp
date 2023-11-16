7ede047856dc3762f2cdd2ae84de96b302df82f8
RealByter <justavishay@gmail.com>
2023-11-16 12:04:26 +0200

    set up firebase functions

--- client/src/firebase/connection.ts
+++ client/src/firebase/connection.ts
@@ -4,15 +4,22 @@ import { getFirestore, connectFirestoreEmulator, collection } from 'firebase/fir
 import { chatFirestoreConverter } from './chat';
 import { userFirestoreConverter } from './user';
 import { messageFirestoreConverter } from './message';
+import { connectFunctionsEmulator, getFunctions } from 'firebase/functions';
 
 const DEV_AUTH_HOST = 'http://127.0.0.1:9099';
 const DEV_FIRESTORE_HOST = '127.0.0.1';
 const DEV_FIRESTORE_PORT = 8080;
+const DEV_FUNCTIONS_HOST = '127.0.0.1';
+const DEV_FUNCTIONS_PORT = 5001;
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY || 'default',
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || 'localhost:9099',
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  // if i forget: find a way to set the correct project id with the firebase emulators
+  projectId:
+    import.meta.env.VITE_FIREBASE_PROJECT_ID === 'your-project-id'
+      ? 'callforhelp-37002'
+      : import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID
@@ -22,10 +29,12 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const firestore = getFirestore(app);
+const functions = getFunctions(app);
 
 if (import.meta.env.DEV) {
   connectAuthEmulator(auth, DEV_AUTH_HOST);
   connectFirestoreEmulator(firestore, DEV_FIRESTORE_HOST, DEV_FIRESTORE_PORT);
+  connectFunctionsEmulator(functions, DEV_FUNCTIONS_HOST, DEV_FUNCTIONS_PORT);
 }
 
 const collections = {
@@ -34,4 +43,4 @@ const collections = {
   messages: collection(firestore, 'messages').withConverter(messageFirestoreConverter)
 };
 
-export { auth, collections };
+export { auth, functions, collections };
---
+++ client/src/firebase/functions/src/index.ts
@@ -0,0 +1,21 @@
+/* eslint-disable max-len */
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import {onCall} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = onCall((request) => {
+  logger.info("Hello logs!", {structuredData: true});
+  logger.log("data: " + request.data);
+  return "Hello from Firebase!";
+});
